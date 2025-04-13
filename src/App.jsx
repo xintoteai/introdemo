d64e5904f72420e622dd51f3b9e38229b7e77961
Xin Heng <xin@tote.ai>
2025-04-13 15:38:04 -0700

    Completed part-2a

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,16 @@
-import { useState } from 'react';
+import Note from './components/Note';
 
-const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
-
-const Display = props => <div>{props.value}</div>
-
-const App = () => {
-  const [value, setValue] = useState(0)
-
-  const setToValue = newValue => {
-    console.log('value now', newValue)
-    setValue(newValue)
-  }
-
+const App = ({ notes }) => {
   return (
     <div>
-      <Display value={value} />
-      <Button onClick={() => setToValue(1000)} text="thousand" />
-      <Button onClick={() => setToValue(0)} text="reset" />
-      <Button onClick={() => setToValue(value + 1)} text="increment" />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map((note) => (
+          <Note key={note.id} note={note} />
+        ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
+
 export default App;
