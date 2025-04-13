adb7a697b7266dc964dbaa48c203e4d713d5cd4f
xintoteai <160626233+xintoteai@users.noreply.github.com>
2025-04-13 14:59:14 -0700

    Completed part 1d (#1)
    * a few lines
    
    * complete it

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,12 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
 
-export default [
-  { ignores: ['dist'] },
-  {
-    files: ['**/*.{js,jsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        ecmaFeatures: { jsx: true },
-        sourceType: 'module',
-      },
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-]
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,24 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
-const Display = ({ counter }) => <div>{counter}</div>
+const Button = (props) => <button onClick={props.onClick}>{props.text}</button>;
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
+const Display = props => <div>{props.value}</div>
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [value, setValue] = useState(0)
 
-  console.log('rendering with counter value', counter)
-
-  const increaseByOne = () => {
-
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
-  }
-
-  const decreaseByOne = () => { 
-
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const setToValue = newValue => {
+    console.log('value now', newValue)
+    setValue(newValue)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <Display value={value} />
+      <Button onClick={() => setToValue(1000)} text="thousand" />
+      <Button onClick={() => setToValue(0)} text="reset" />
+      <Button onClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
-} 
-
-export default App
+}
+export default App;
