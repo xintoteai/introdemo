6565fb2edcb43ca308b0a59a403d45df179147a0
Xin Heng <xin.heng@gmail.com>
2025-04-14 22:34:33 -0700

    continue with part 2b

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,33 @@ import { useState } from 'react';
 import Note from './components/Note';
 
 const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note...') 
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState('a new note ...');
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
-    event.preventDefault()
-    console.log('button clicked', event.target)
-  }
-  
+    event.preventDefault();
+    console.log('button clicked', event.target);
+  };
+
   const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
-  }
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important === true);
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
-        )}
+        ))}
       </ul>
-
-      <form onSubmit={addNote}>
-      <input 
-        value={newNote} 
-        onChange={handleNoteChange}
-        />
-        <button type="submit">save</button>
-      </form>   
+      // ...
     </div>
-  )
+  );
 };
 
 export default App;
